fdd1aeaa3a77d08cefa3ab28a5f7492608b47fb7
Umar Hamid <uh_2001@hotmail.com>
2024-11-21 15:49:49 +0000

    Update Task_3_6.sql

--- Task_3_6.sql
+++ Task_3_6.sql
@@ -3,6 +3,9 @@
 --SELECT MAX(LENGTH("day")) FROM dim_date_times
 --SELECT MAX(LENGTH("time_period")) FROM dim_date_times
 
+
+--Cast the correct data types to dim_date_times table
+
 ALTER TABLE dim_date_times
 ALTER COLUMN month TYPE VARCHAR(2) USING month::VARCHAR(2),
 ALTER COLUMN year TYPE VARCHAR(4) USING year::VARCHAR(4),
