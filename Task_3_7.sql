261e4ddc1bac863856c1d6206ea5942b2afe86b3
Umar Hamid <uh_2001@hotmail.com>
2024-11-21 15:50:14 +0000

    Update Task_3_7.sql

--- Task_3_7.sql
+++ Task_3_7.sql
@@ -1,6 +1,8 @@
 --SELECT MAX(LENGTH(card_number)) FROM dim_card_details_new;
 --SELECT MAX(LENGTH(expiry_date)) FROM dim_card_details_new;
 
+--Cast correct data types to dim_card_details table
+
 ALTER TABLE dim_card_details_new
 ALTER COLUMN card_number TYPE VARCHAR(22),
 ALTER COLUMN expiry_date TYPE VARCHAR(5),
